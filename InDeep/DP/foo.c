36255f43fc033b1dde358b8510c0664a9e782a49
lvtao <tao.lv03@ele.me>
2019-01-13 23:17:22 +0800

    add makefile

---
+++ InDeep/DP/foo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int i;
+    puts("Count 1 to 10: ");
+    for(i = 1; i<= 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
