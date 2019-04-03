6ec7db481d8811466605a35b0bca304cf111e2a1
lvtao <tao.lv03@ele.me>
2019-04-03 22:33:51 +0800

    add stack and evaluate

---
+++ Algorithms/src/test/java/basic/EvaluateTest.java
@@ -0,0 +1,11 @@
+package basic;
+
+import org.junit.jupiter.api.Test;
+
+class EvaluateTest {
+
+    @Test
+    void evaluate() {
+        System.out.println(Evaluate.evaluate("(1+1)+(1*2)"));
+    }
+}
