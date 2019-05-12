89ecbdf1b69263d3ad0da82bf27fd64f544d8c0e
lrtzlvtao <547617251lrtz@gmail.com>
2019-05-12 22:58:08 +0800

    remove k digit fucked

--- leetcode/removeKDigits.go
+++ leetcode/removeKDigits.go
@@ -1,26 +1,33 @@
 package leetcode
 
+import (
+	"strings"
+)
+
 /*
 	从一个整数中删除K个数字，使得新数字尽可能小
 
 	从高位开始便历，如果一个数字大于右侧的数字，则删除
 */
 
-func removeKDigits(arr []int, k int) []int {
-	tmp := arr
-	for i := 0; i < k; i++ {
-		if len(tmp) <= 1 {
-			return []int{}
-		}
-		for j := 0; j <= len(tmp)-2; j++ {
-			if tmp[j] > tmp[j+1] {
-				var res []int
-				res = append(res, tmp[0:j]...)
-				res = append(res, tmp[j+1:]...)
-				tmp = res
-				break
+func removeKDigits(num string, k int) string {
+	if len(num) == 0 || (len(num) == 1 && k > 0) {
+		return ""
+	} else if k == 0 {
+		return num
+	}
+
+	arr := strings.Split(num, "")
+	res := make([]string, len(arr))
+
+	for i := 0; i < len(arr); i++ {
+		if k > 0 {
+			for j := 0; j < len(res); j++ {
+
 			}
 		}
+		res[i] = arr[i]
 	}
-	return tmp
+
+	return strings.Join(res, "")
 }
--- leetcode/removeKDigits_test.go
+++ leetcode/removeKDigits_test.go
@@ -6,19 +6,25 @@ import (
 )
 
 func TestRemoveKDigits(t *testing.T) {
-	testArr := []int{5, 4, 1, 2, 7, 0, 9, 3, 6}
+	testArr := "541270936"
 	res := removeKDigits(testArr, 1)
 	fmt.Println(res)
 }
 
 func TestRemoveKDigits2(t *testing.T) {
-	testArr := []int{1, 5, 9, 3, 2, 1, 2}
+	testArr := "1593212"
 	res := removeKDigits(testArr, 3)
 	fmt.Println(res)
 }
 
 func TestRemoveKDigits3(t *testing.T) {
-	testArr := []int{1, 0}
+	testArr := "10"
+	res := removeKDigits(testArr, 2)
+	fmt.Println(res)
+}
+
+func TestRemoveKDigits4(t *testing.T) {
+	testArr := "1111114132"
 	res := removeKDigits(testArr, 2)
 	fmt.Println(res)
 }
