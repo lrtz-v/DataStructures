ba90a5e4398709e88cb8b18c5f267642257adca8
lrtzlvtao <547617251lrtz@gmail.com>
2019-05-14 00:54:07 +0800

    remove k digit fucked

--- leetcode/removeKDigits.go
+++ leetcode/removeKDigits.go
@@ -1,16 +1,59 @@
 package leetcode
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 )
 
 /*
 	从一个整数中删除K个数字，使得新数字尽可能小
 
-	从高位开始便历，如果一个数字大于右侧的数字，则删除
+	从高位开始遍历，如果一个数字大于右侧的数字，则删除
 */
 
 func removeKDigits(num string, k int) string {
+	if len(num) == 0 || (len(num) == 1 && k > 0) {
+		return ""
+	} else if k == 0 {
+		return num
+	}
+
+	arr := strings.Split(num, "")
+	res := make([]string, len(arr))
+	index := 0
+	for i := 0; i < len(arr); i++ {
+		res[index] = arr[i]
+		if index == 0 || k == 0 {
+			index++
+			continue
+		}
+
+		for j := i; j > 0 && k > 0; j-- {
+			val, _ := strconv.Atoi(res[j])
+			lastVal, _ := strconv.Atoi(res[j-1])
+			if lastVal > val {
+				res[j-1], res[j] = res[j], ""
+				index--
+				k--
+			}
+		}
+		//
+		//fmt.Printf("val: %d, lastVal: %d\n", val, lastVal)
+		//if lastVal > val {
+		//	res[i-1] = ""
+		//	res[i] = arr[i]
+		//	k--
+		//} else {
+		//	res[i] = arr[i]
+		//}
+		fmt.Println(res)
+	}
+
+	return strings.Join(res, "")
+}
+
+func removeKDigits2(num string, k int) string {
 	if len(num) == 0 || (len(num) == 1 && k > 0) {
 		return ""
 	} else if k == 0 {
--- leetcode/removeKDigits_test.go
+++ leetcode/removeKDigits_test.go
@@ -1,30 +1,41 @@
 package leetcode
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestRemoveKDigits(t *testing.T) {
+	t.Skip()
 	testArr := "541270936"
 	res := removeKDigits(testArr, 1)
-	fmt.Println(res)
+	if res != "41270936" {
+		t.Errorf("Expected: %s, Got: %s", "41270936", res)
+	}
 }
 
 func TestRemoveKDigits2(t *testing.T) {
+	//t.Skip()
 	testArr := "1593212"
 	res := removeKDigits(testArr, 3)
-	fmt.Println(res)
+	if res != "1212" {
+		t.Errorf("Expected: %s, Got: %s", "1212", res)
+	}
 }
 
 func TestRemoveKDigits3(t *testing.T) {
+	t.Skip()
 	testArr := "10"
 	res := removeKDigits(testArr, 2)
-	fmt.Println(res)
+	if res != "" {
+		t.Errorf("Expected: %s, Got: %s", "", res)
+	}
 }
 
 func TestRemoveKDigits4(t *testing.T) {
+	t.Skip()
 	testArr := "1111114132"
 	res := removeKDigits(testArr, 2)
-	fmt.Println(res)
+	if res != "11111112" {
+		t.Errorf("Expected: %s, Got: %s", "11111112", res)
+	}
 }
