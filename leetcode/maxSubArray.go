2895ec861b08a13bbc3660d03a5e93b0dc3ddb18
lrtzlvtao <547617251lrtz@gmail.com>
2019-09-28 22:54:25 +0800

    maxSubArray

---
+++ leetcode/maxSubArray.go
@@ -0,0 +1,28 @@
+package leetcode
+
+func maxSubArray(nums []int) int {
+
+	n := len(nums)
+
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return nums[0]
+	}
+
+	max := nums[0]
+	sum := 0
+
+	for _, v := range nums {
+		if sum > 0 {
+			sum += v
+		} else {
+			sum = v
+		}
+		if sum > max {
+			max = sum
+		}
+	}
+
+	return max
+}
---
+++ leetcode/maxSubArray_test.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMaxSubArray(t *testing.T) {
+	l := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	max := maxSubArray(l)
+	fmt.Println(max)
+	if max != 6 {
+		t.Fatal("Max Error!")
+	}
+}
