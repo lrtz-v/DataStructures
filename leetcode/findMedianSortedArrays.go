f6c8fd55f9150a57aaca1c453292e948886e5438
lrtzlvtao <547617251lrtz@gmail.com>
2020-02-29 17:33:49 +0800

    del wrong method in findMedianSortedArrays

--- leetcode/findMedianSortedArrays.go
+++ leetcode/findMedianSortedArrays.go
@@ -13,38 +13,5 @@ package leetcode
 // The median is (2 + 3)/2 = 2.5
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	var tmpArr []int
-	tmpArr = append(tmpArr, nums1...)
-	tmpArr = append(tmpArr, nums2...)
-
-	// max, min
-	max := 0
-	min := 0
-	for _, v := range tmpArr {
-		if v > max {
-			max = v
-		}
-		if v < min {
-			min = v
-		}
-	}
-
-	indexArr := make([]int, max-min+1)
-	var resArr []int
-	for _, data := range tmpArr {
-		indexArr[data-min]++
-	}
-
-	for index, count := range indexArr {
-		for j := 0; j < count; j++ {
-			resArr = append(resArr, index+min)
-		}
-	}
-
-	if len(resArr)%2 == 0 {
-		m := len(resArr)/2 - 1
-		return float64(resArr[m]+resArr[m+1]) / 2
-	}
-	m := (len(resArr)+1)/2 - 1
-	return float64(resArr[m])
+	return 0.0
 }
