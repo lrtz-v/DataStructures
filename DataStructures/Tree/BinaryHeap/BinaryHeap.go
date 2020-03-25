93c0230b01be6c393e25830ff0935b70eddc1810
lrtzlvtao <547617251lrtz@gmail.com>
2020-03-25 22:03:07 +0800

    add binary heap sort

--- DataStructures/Tree/BinaryHeap/BinaryHeap.go
+++ DataStructures/Tree/BinaryHeap/BinaryHeap.go
@@ -1,172 +1,78 @@
 package binaryheap
 
-/*
+import (
+	"strconv"
+	"strings"
+)
+
+/**
 二叉堆
 构建时间复杂度：O(n)
 */
 
-// BinaryHeap 最大二叉堆
-
-// findFirstEqual
-func findFirstEqual(a []int, n int) int {
-
-	low := 0
-	high := len(a) - 1
-
-	for low <= high {
-		mid := low + (high-low)>>1
-
-		if a[mid] > n {
-			high = mid - 1
-		} else if a[mid] < n {
-			low = mid + 1
-		} else {
-			if mid == 0 || a[mid-1] != n {
-				return mid
-			}
-			high = mid - 1
-		}
-	}
-	return -1
+// BinaryHeap 二叉堆
+type BinaryHeap struct {
+	heap []int
+	len int
 }
 
-// findLastEqual
-func findLastEqual(a []int, n int) int {
+func CreateBinaryHeap(numbers []int) (heap *BinaryHeap) {
+	heap = &BinaryHeap{heap:numbers, len:len(numbers)}
+	// 构建堆O(n), 满足堆中的每个节点的值必须大于等于其子树中每个节点的值
+	heap.buildHeap()
 
-	low := 0
-	high := len(a) - 1
+	// 排序
+	heap.sort()
 
-	for low <= high {
-		mid := low + (high-low)>>1
-
-		if a[mid] > n {
-			high = mid - 1
-		} else if a[mid] < n {
-			low = mid + 1
-		} else {
-			if mid == n - 1 || a[mid+1] != n {
-				return mid
-			}
-			high = mid - 1
-		}
-	}
-	return -1
+	return
 }
 
-// findFirstBE (>=)
-func findFirstBE(a []int, n int) int {
-
-	low := 0
-	high := len(a) - 1
-
-	for low <= high {
-		mid := low + (high-low)>>1
-
-		if a[mid] < n {
-			low = mid + 1
-		} else {
-			if mid == 0 || a[mid-1] < n {
-				return mid
-			}
-			high = mid - 1
-		}
+func (bh *BinaryHeap) buildHeap() {
+	// 从非叶子节点开始
+	for i := bh.len / 2; i >= 0; i-- {
+		bh.heapify(bh.len, i)
 	}
-	return -1
 }
 
-// findLastLE (<=)
-func findLastLE(a []int, n int) int {
-
-	low := 0
-	high := len(a) - 1
-
-	for low <= high {
-		mid := low + (high-low)>>1
-
-		if a[mid] > n {
-			high = mid - 1
-		} else {
-			if mid == n - 1 || a[mid+1] > n {
-				return mid
-			}
-			low = mid + 1
+func (bh *BinaryHeap) heapify(n, i int) {
+	for {
+		maxPos := i
+		leftIndex := i*2+1
+		rightIndex := i*2+2
+		// 父节点必须大于左右子节点
+		if leftIndex < n && bh.less(i, leftIndex) {
+			maxPos = leftIndex
 		}
-	}
-	return -1
-}
-
-// findEqualInCycleArr: [4, 5, 6, 7, 1, 2, 3]
-func findEqualInCycleArr(nums []int, target int) int {
-
-    if len(nums) == 0{
-        return -1
-    } else if len(nums) == 1{
-        if nums[0] == target {
-            return 0
-        }
-        return -1
-    }
-
-	p := rotationIndex(nums)
-
-	low := 0
-	high := len(nums) - 1
-
-	if p != -1 {
-		if nums[p] == target {
-			return p
+		if rightIndex < n && bh.less(maxPos,rightIndex) {
+			maxPos = rightIndex
 		}
-		if nums[len(nums)-1] >= target {
-			low = p + 1
-			high = len(nums)-1
-		} else if nums[0] <= target {
-			low = 0
-			high = p
-		} else {
-			return -1
-		}
-	}
-
-	return targetIndex(nums,low, high, target)
-}
-
-func rotationIndex(nums []int) int {
-
-	low := 0
-	high := len(nums) - 1
-	p := -1
-
-	for low < high {
-		mid := low + (high-low)>>1
-
-		if mid != len(nums) -1 && nums[mid] > nums[mid+1] {
-			p = mid
+		if maxPos == i {
 			break
-		} else if mid != 0 && nums[mid - 1] > nums[mid] {
-			p = mid - 1
-			break
-		} else if nums[mid] > nums[low] {
-			low = mid + 1
-		} else if nums[mid] < nums[high] {
-			high = mid - 1
 		}
+		bh.swap(i, maxPos)
+		i = maxPos
 	}
-
-	return p
 }
 
-func targetIndex(nums []int, low, high, target int) int {
+func (bh *BinaryHeap) swap(i, j int) {
+	bh.heap[i], bh.heap[j] = bh.heap[j], bh.heap[i]
+}
 
-	for low <= high {
-		mid := low + (high-low)>>1
+func (bh *BinaryHeap) less(i, j int) bool {
+	return bh.heap[i] < bh.heap[j]
+}
 
-		if nums[mid] == target {
-			return mid
-		} else if nums[mid] > target {
-			high = mid - 1
-		} else if nums[mid] < target {
-			low = mid + 1
-		}
+func (bh *BinaryHeap) sort() {
+	for i := bh.len-1; i > 0; i-- {
+		bh.swap(i, 0)
+		bh.heapify(i, 0)
 	}
-	return -1
+}
+
+func (bh *BinaryHeap) toString() string {
+	var str []string
+	for _, v := range bh.heap {
+		str = append(str, strconv.Itoa(v))
+	}
+	return strings.Join(str, ",")
 }
--- DataStructures/Tree/BinaryHeap/BinaryHeap_test.go
+++ DataStructures/Tree/BinaryHeap/BinaryHeap_test.go
@@ -4,92 +4,19 @@ import (
 	"testing"
 )
 
-func TestFindFirstEqual(t *testing.T) {
+
+func TestCreate(t *testing.T) {
 	// t.Skip()
-	testData := []int{3, 6, 7, 8, 8, 10}
-	i := findFirstEqual(testData,8)
-	if i != 3 {
-		t.Fatal("TestFindFirstEqual Failed")
+	testData := []int{57, 40, 38, 11, 13, 34, 48, 75, 6, 19, 9, 7}
+	aim := []int{6, 7, 9, 11, 13, 19, 34, 38, 40, 48, 57, 75}
+	bh := CreateBinaryHeap(testData)
+	if len(testData) != len(bh.heap) || len(aim) != len(bh.heap) {
+		t.Fatal("elements missing!")
 	}
-}
-
-func TestFindLastEqual(t *testing.T) {
-	// t.Skip()
-	testData := []int{3, 6, 7, 8, 8, 10}
-	i := findLastEqual(testData, 8)
-	if i != 4 {
-		t.Fatal("TestFindLastEqual Failed")
+	for i := 0; i< len(aim); i++ {
+		if aim[i] != bh.heap[i] {
+			t.Fatal("init failed!")
+		}
 	}
-}
-
-func TestFindFirstBE(t *testing.T) {
-	// t.Skip()
-	testData := []int{1, 3, 5, 7, 7, 9}
-	i := findFirstBE(testData, 6)
-	if i != 3 {
-		t.Fatal("TestFindFirstBE Failed")
-	}
-
-	i = findFirstBE(testData, 5)
-	if i != 2 {
-		t.Fatal("TestFindFirstBE Failed")
-	}
-
-	i = findFirstBE(testData, 7)
-	if i != 3 {
-		t.Fatal("TestFindFirstBE Failed")
-	}
-}
-
-func TestFindLastLE(t *testing.T) {
-	// t.Skip()
-	testData := []int{1, 3, 5, 7, 7, 9}
-	i := findLastLE(testData, 8)
-	if i != 4 {
-		t.Fatal("TestFindLastLE Failed")
-	}
-
-	i = findLastLE(testData, 5)
-	if i != 2 {
-		t.Fatal("TestFindLastLE Failed")
-	}
-
-	i = findLastLE(testData, 6)
-	if i != 2 {
-		t.Fatal("TestFindLastLE Failed")
-	}
-}
-
-func TestFindEqualInCycleArr(t *testing.T) {
-	// t.Skip()
-
-	testData2 := []int{4, 5, 6, 7, 1, 2, 3}
-	i := findEqualInCycleArr(testData2, 2)
-	if i != 5 {
-		t.Fatal("TestFindEqualInCycleArr Failed")
-	}
-
-	testData2 = []int{4, 5, 6, 1, 2, 3}
-	i = findEqualInCycleArr(testData2, 2)
-	if i != 4 {
-		t.Fatal("TestFindEqualInCycleArr Failed")
-	}
-
-	testData := []int{5, 1, 3}
-	i = findEqualInCycleArr(testData, 5)
-	if i != 0 {
-		t.Fatal("TestFindEqualInCycleArr Failed")
-	}
-
-	testData = []int{1, 3}
-	i = findEqualInCycleArr(testData, 0)
-	if i != -1 {
-		t.Fatal("TestFindEqualInCycleArr Failed")
-	}
-
-	//testData = []int{1, 3, 5}
-	//i = findEqualInCycleArr(testData, 0)
-	//if i != -1 {
-	//	t.Fatal("TestFindEqualInCycleArr Failed")
-	//}
+	t.Log(bh.toString())
 }
