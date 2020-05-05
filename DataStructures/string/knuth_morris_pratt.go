03e2235eab96fd571bb0465649af67c61b59df4e
lrtzlvtao <547617251lrtz@gmail.com>
2020-05-05 14:04:42 +0800

    add string search docs

--- DataStructures/string/knuth_morris_pratt.go
+++ DataStructures/string/knuth_morris_pratt.go
@@ -2,25 +2,25 @@ package string
 
 func generateNext(p string) []int {
 	next := make([]int, len(p))
-    pLen := len(p)
-    next[0] = -1
-    k := -1
-    j := 0
-    for j < pLen - 1 {
-        //p[k]表示前缀，p[j]表示后缀
-        if k == -1 || p[j] == p[k] {
-            k++
-            j++
+	pLen := len(p)
+	next[0] = -1
+	k := -1
+	j := 0
+	for j < pLen-1 {
+		//p[k]表示前缀，p[j]表示后缀
+		if k == -1 || p[j] == p[k] {
+			k++
+			j++
 			if p[j] != p[k] {
-            	next[j] = k
+				next[j] = k
 			} else {
 				//因为不能出现p[j] = p[ next[j ]]，所以当出现时需要继续递归，k = next[k] = next[next[k]]
-                next[j] = next[k]
+				next[j] = next[k]
 			}
-        } else {
-            k = next[k]
-        }
-    }
+		} else {
+			k = next[k]
+		}
+	}
 	return next
 }
 
@@ -39,14 +39,14 @@ func kmp(s, p string) int {
 			j++
 		} else {
 			// ②如果j != -1，且当前字符匹配失败（即S[i] != P[j]），则令 i 不变，j = next[j]
-            // next[j]即为j所对应的next值
-            j = next[j]
+			// next[j]即为j所对应的next值
+			j = next[j]
 		}
 	}
 
 	if j == pLen {
 		return i - j
-	} else {
-		return -1
 	}
+	return -1
+
 }
---
+++ DataStructures/string/readme.md
@@ -0,0 +1,20 @@
+[!DOC]
+
+# 字符串匹配
+
+## BF(Brute Force)
+
+- 也叫作暴力匹配算法，也叫朴素匹配算法
+- 暴力查询
+
+## RK(Rabin-Karp)
+
+- 通过哈希算法对主串中的 n-m+1 个子串分别求哈希值，然后逐个与模式串的哈希值比较大小
+
+## KMP(Knuth-Morris-Pratt)
+
+## BM（Boyer-Moore）
+
+- [boyer_moore](http://www.cs.jhu.edu/~langmea/resources/lecture_notes/boyer_moore.pdf)
+
+## Sunday
