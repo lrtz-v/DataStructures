a71cda06a08fc91028fced9f6baa6f4e35e4f47a
lrtz-v <547617251lrtz@gmail.com>
2020-05-13 18:08:26 +0800

    leetcode:levelOrder

---
+++ leetcode/levelOrder/levelorder_test.go
@@ -0,0 +1,24 @@
+package levelorder
+
+import "testing"
+
+func TestLevelOrder(t *testing.T) {
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+
+	levelOrder(root)
+
+}
