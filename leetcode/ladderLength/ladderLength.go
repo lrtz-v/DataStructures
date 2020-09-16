11b5524c4f54831c35403bd8e2586c4794714f74
lrtz <547617251lrtz@gmail.com>
2020-09-16 21:17:11 +0800

    leetcode::ladderLength

---
+++ leetcode/ladderLength/ladderLength.go
@@ -0,0 +1,16 @@
+package ladderLength
+
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+
+	wordMap := make(map[string]int, len(wordList))
+	for i, v := range wordList {
+		wordMap[v] = i
+	}
+
+	if _, ok := wordMap[endWord]; !ok {
+		return 0
+	}
+
+	
+
+}
