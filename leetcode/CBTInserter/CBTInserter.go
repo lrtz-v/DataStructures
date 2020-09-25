c179f21d134ffe06ec10401fd67366c80d10326f
lt177995 <tao.lv@ele-4.local>
2020-09-25 15:13:45 +0800

    Dleetcode::CBTInserter

---
+++ leetcode/CBTInserter/CBTInserter.go
@@ -0,0 +1,30 @@
+package CBTInserter
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type CBTInserter struct {
+	root *TreeNode
+}
+
+func Constructor(root *TreeNode) CBTInserter {
+
+}
+
+func (this *CBTInserter) Insert(v int) int {
+
+}
+
+func (this *CBTInserter) Get_root() *TreeNode {
+
+}
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Insert(v);
+ * param_2 := obj.Get_root();
+ */
